9081c7ef83ac45613ce46b2b32ffb89d8f751132
Tomasz Pluskiewicz <tpluscode@users.noreply.github.com>
2025-07-30 10:57:04 +0200

    refactor: extract card component

---
+++ opendata.swiss/ui/app/components/OdsCard.vue
@@ -0,0 +1,42 @@
+<script setup>
+const { title } = defineProps({
+  title: {
+    type: String,
+    required: true
+  },
+})
+
+</script>
+
+<template>
+  <div class="card card--universal card--clickable">
+    <div class="card__content">
+      <div class="card__body">
+        <p class="meta-info">
+          <slot name="top-meta" />
+        </p>
+        <div class="card__title">
+          <h3>{{ title }}</h3>
+        </div>
+        <slot/>
+
+        <p class="meta-info">
+          <slot name="bottom-meta" />
+        </p>
+        <div class="card__content-icons">
+          <slot name="icons"/>
+        </div>
+      </div>
+      <div class="card__footer">
+        <div class="card__footer__info">
+          <p class="meta-info">
+            <slot name="footer-info"/>
+          </p>
+        </div>
+        <div class="card__footer__action">
+          <slot name="footer-action"/>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
