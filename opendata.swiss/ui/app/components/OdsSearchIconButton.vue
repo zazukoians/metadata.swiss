92c9710c8d4ede965cd22b1d4ef728d36252208b
Tomasz Pluskiewicz <tpluscode@users.noreply.github.com>
2025-08-08 15:30:02 +0200

    refactor: use SvgIcon in all places

--- opendata.swiss/ui/app/components/OdsSearchIconButton.vue
+++ opendata.swiss/ui/app/components/OdsSearchIconButton.vue
@@ -5,9 +5,7 @@
   class="btn btn--bare btn--lg btn--icon-only"
   :aria-label="t('message.dataset_search.search_button')"
   >
-    <svg viewBox="0 0 24 24" enable-background="new 0 0 24 24" xmlns="http://www.w3.org/2000/svg" class="icon icon--base icon--Search btn__icon">
-      <path xmlns="http://www.w3.org/2000/svg" d="m13.3 12.8c1.9-2.2 1.7-5.6-.5-7.5s-5.6-1.7-7.5.5-1.7 5.6.5 7.5c2 1.7 4.9 1.7 6.9 0l6 6 .5-.5zm-4 1c-2.5 0-4.5-2-4.5-4.5s2-4.5 4.5-4.5 4.5 2 4.5 4.5-2 4.5-4.5 4.5z" />
-    </svg>
+    <SvgIcon icon="Search" role="btn" />
     <span class="btn__text">{{ t('message.dataset_search.search_button') }}</span>
   </button>
 </template>
--- opendata.swiss/ui/pages/blog/[[page]].vue
+++ opendata.swiss/ui/pages/blog/[[page]].vue
@@ -1,6 +1,7 @@
 <script setup>
 import OdsPage from "../../app/components/OdsPage.vue";
 import {homePageBreadcrumb} from "../../app/composables/breadcrumbs.js";
+import SvgIcon from "../../app/components/SvgIcon.vue";
 
 const route = useRoute()
 const {locale, t} = useI18n()
@@ -68,11 +69,7 @@ const breadcrumbs = [
                   <NuxtLinkLocale
                     :to="{ name: 'blog-year-month-slug', params: { year: post.year, month: post.month, slug: post.slug } }"
                     type="false" class="btn btn--outline btn--icon-only" aria-label="false">
-                    <svg viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"
-                         class="icon icon--base icon--ArrowRight btn__icon">
-                      <path xmlns="http://www.w3.org/2000/svg"
-                            d="m16.444 19.204 4.066-7.044-4.066-7.044-.65.375 3.633 6.294h-15.187v.75h15.187l-3.633 6.294z"></path>
-                    </svg>
+                    <SvgIcon icon="ArrowRight" role="btn" />
                     <span class="btn__text">Weiterlesen</span>
                   </NuxtLinkLocale>
                 </template>
