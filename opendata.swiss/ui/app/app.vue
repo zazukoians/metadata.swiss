23dfa487d380b5846460d3f936cdf9a4f69dc3ff
Hofstetter Benjamin <benjamin.hofstetter@zazuko.com>
2025-09-11 14:21:40 +0200

    favicon

--- opendata.swiss/ui/app/app.vue
+++ opendata.swiss/ui/app/app.vue
@@ -42,7 +42,18 @@ const isMobileMenuOpen = ref(false);
 useHead({
   bodyAttrs: {
     class: 'body--ods-brand'
-  }
+  },
+  link: [
+    { rel: 'icon', type: 'image/png', sizes: '96x96', href: '/favicon-96x96.png' },
+    { rel: 'icon', type: 'image/svg+xml', href: '/favicon.svg' },
+    { rel: 'shortcut icon', href: '/favicon.ico' },
+    { rel: 'apple-touch-icon', sizes: '180x180', href: '/apple-touch-icon.png' },
+    { rel: 'manifest', href: '/site.webmanifest' },
+  ],
+  meta: [
+    { name: 'apple-mobile-web-app-title', content: 'opendata.swiss' },
+    // you can add other default meta tags, e.g. description, viewport, etc
+  ]
 })
 
 function mobileMenuOpened(value: boolean) {
